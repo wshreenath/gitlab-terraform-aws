83f27530be95d291b45ca48bfa62e41248c44583
wshreenath <148483485+wshreenath@users.noreply.github.com>
2025-12-27 15:28:10 +0530

    Delete terraform/backend.tf

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "gitlab-terraform-state-prod"
-    key            = "dev/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
